02216e38c3259e21ea737b5a63347994befc261c
paviotti <crpaviotti@hotmail.com>
2020-01-08 16:25:06 -0300

    Git pelo Eclipse

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -5,6 +5,7 @@ public class programa {
 	public static void main(String[] args) {
 		System.out.println("Primeira postagem Git");
 		System.out.println("Bom dia");
+		System.out.println("Usando o Eclipse para Git");
 
 	}
 
